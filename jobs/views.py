5911e1229fe7a75de44c2ea0f6bf17ee9539c071
zq912144658 <912144658@qq.com>
2020-11-20 11:24:00 +0600

    07 commit

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from jobs.models import Job
+from jobs.models import Cities,JobTYpes
+
+def joblist(request):
+    job_list = Job.objects.order_by('job_type')
+    template = loader.get_template('joblist.html')
+    context = {'job_list':job_list}
+    
+    for job in job_list:
+        job.city_name = Cities[job.job_city][1]
+        job.job_type = JobTYpes[job.job_type][1]
+    
+    return HttpResponse(template.reder(context))
